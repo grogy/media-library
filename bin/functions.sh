20ba8375b76de8e7032eb0a6238cd2d20b48e09e
Václav Makeš <info@makes.cz>
2017-02-27 19:13:21 +0100

    Added helpers in Bash for easy work with containers

---
+++ bin/functions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+APP_CONTAINER='web'
+
+function renderCommands() {
+    echo -e "${GREEN}Console tool for MediaLibrary${NC}"
+    echo ""
+    echo -e "${ORANGE}Available commands for localhost environment:${NC}"
+    echo -e " ${GREEN}start${NC}   Start and setup development environment."
+    echo -e " ${GREEN}stop${NC}    Stop development environment."
+    echo -e " ${GREEN}in${NC}      Go into a container."
+    echo ""
+    exit 0;
+}
+
+
+function start() {
+    docker-compose up -d --build;
+    echo "== All done, happy coding. ==";
+}
+
+
+function stop() {
+    docker-compose down;
+}
+
+
+function goIntoContainer() {
+    docker exec -t -i ${APP_CONTAINER} bash;
+}
