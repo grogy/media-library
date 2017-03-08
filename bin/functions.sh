3f264ae612936f643fad4e4f855270951c8281ca
Václav Sobotka <sobotka.vaclav@gmail.com>
2017-03-08 17:11:51 +0100

    User specific attaching to container

--- bin/functions.sh
+++ bin/functions.sh
@@ -12,7 +12,8 @@ function renderCommands() {
     echo -e "${ORANGE}Available commands for localhost environment:${NC}"
     echo -e " ${GREEN}start${NC}   Start and setup development environment."
     echo -e " ${GREEN}stop${NC}    Stop development environment."
-    echo -e " ${GREEN}in${NC}      Go into a container."
+    echo -e " ${GREEN}in${NC}      Go into a container as current user."
+    echo -e " ${GREEN}indef${NC}   Go into a container as default user."
     echo ""
     exit 0;
 }
@@ -20,7 +21,6 @@ function renderCommands() {
 
 function start() {
     docker-compose up --build;
-    echo "== All done, happy coding. ==";
 }
 
 
@@ -29,6 +29,11 @@ function stop() {
 }
 
 
-function goIntoContainer() {
+function goIntoContainerCurrentUser() {
+    docker exec -t -i -u `id -u $UID` ${APP_CONTAINER} bash;
+}
+
+
+function goIntoContainerDefaultUser() {
     docker exec -t -i ${APP_CONTAINER} bash;
 }
